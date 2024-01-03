b563240f0b536d13de4fb396922a8914ceafc026
InfintyJer <89025169+InfinityJer@users.noreply.github.com>
2024-01-03 20:40:43 +0800

    Update 6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,15 +1,8 @@
 // 6-final-user.js
+
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-/**
- * Handles profile signup by calling signUpUser and uploadPhoto functions.
- * Returns an array with the status and value or error of the settled promises.
- * @param {string} firstName - The first name of the user.
- * @param {string} lastName - The last name of the user.
- * @param {string} fileName - The name of the file to be processed.
- * @returns {Promise} - Promise that resolves with an array of settled promises' status and values/errors.
- */
 export default async function handleProfileSignup(firstName, lastName, fileName) {
   return Promise
     .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
