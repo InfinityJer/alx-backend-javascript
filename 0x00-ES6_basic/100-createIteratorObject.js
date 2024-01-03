d9867292f445bd98cc788ed2207b94021dbff285
InfintyJer <89025169+InfinityJer@users.noreply.github.com>
2024-01-03 19:05:07 +0800

    Update 100-createIteratorObject.js

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,34 +1,13 @@
 // 100-createIteratorObject.js
 
 export default function createIteratorObject(report) {
-  const iterator = {
-    [Symbol.iterator]() {
-      let currentDepartmentIndex = 0;
-      let currentEmployeeIndex = 0;
-      const departments = Object.keys(report.allEmployees);
-
-      return {
-        next() {
-          if (currentDepartmentIndex < departments.length) {
-            const currentDepartment = departments[currentDepartmentIndex];
-            const employees = report.allEmployees[currentDepartment];
-
-            if (currentEmployeeIndex < employees.length) {
-              const employee = employees[currentEmployeeIndex];
-              currentEmployeeIndex++;
-              return { value: employee, done: false };
-            } else {
-              currentEmployeeIndex = 0;
-              currentDepartmentIndex++;
-              return next();
-            }
-          } else {
-            return { done: true };
-          }
-        },
-      };
-    },
-  };
-
-  return iterator;
+  return (function* _() {
+    // eslint-disable-next-line no-restricted-syntax
+    for (const department of Object.values(report.allEmployees)) {
+      // eslint-disable-next-line no-restricted-syntax
+      for (const employee of department) {
+        yield employee;
+      }
+    }
+  }());
 }
