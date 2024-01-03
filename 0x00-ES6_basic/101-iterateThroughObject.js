87144e2d10c63aa56521dbefe2249dc51711ea17
InfinityJer <jclaris6@gmail.com>
2024-01-03 11:01:40 +0000

    Iterate through object

---
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -0,0 +1,11 @@
+// 101-iterateThroughObject.js
+
+export default function iterateThroughObject(reportWithIterator) {
+  const resultArray = [];
+
+  for (const employee of reportWithIterator) {
+    resultArray.push(employee);
+  }
+
+  return resultArray.join(' | ');
+}
