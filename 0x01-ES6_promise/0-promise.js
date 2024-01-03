8dea24df1d650bbcc15e86c013df094765bbef33
InfinityJer <jclaris6@gmail.com>
2024-01-03 12:08:36 +0000

    initial commit

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,10 @@
+// 0-promise.js
+function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+    // You can perform asynchronous operations here
+    // For simplicity, let's resolve the Promise immediately
+    resolve('API response data');
+  });
+}
+
+export default getResponseFromAPI;
