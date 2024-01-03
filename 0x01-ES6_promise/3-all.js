54678b5827db0a6b28fa7cfae5e376a3bc794226
InfintyJer <89025169+InfinityJer@users.noreply.github.com>
2024-01-03 20:34:31 +0800

    Update 3-all.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,26 +1,12 @@
 // 3-all.js
+
 import { uploadPhoto, createUser } from './utils';
 
-/**
- * Collectively resolves promises from uploadPhoto and createUser.
- * Logs body, firstName, and lastName to the console on successful resolution.
- * Logs 'Signup system offline' in case of an error.
- */
-function handleProfileSignup() {
-  // Use Promise.all to handle multiple promises
-  Promise.all([uploadPhoto(), createUser()])
-    .then((results) => {
-      // Destructure the results array
-      const [photoResponse, userResponse] = results;
-
-      // Log the relevant data to the console
-      console.log(`${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`);
+export default function handleProfileSignup() {
+  return Promise
+    .all([uploadPhoto(), createUser()])
+    .then((res) => {
+      console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`);
     })
-    .catch((error) => {
-      // Log an error message in case of any error
-      console.error('Signup system offline');
-    });
+    .catch(() => console.log('Signup system offline'));
 }
-
-// Export the function for external use
-export default handleProfileSignup;
