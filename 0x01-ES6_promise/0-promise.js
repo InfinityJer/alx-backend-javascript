f0feacf31637f106131de2ebbf6a1afc16a80660
InfintyJer <89025169+InfinityJer@users.noreply.github.com>
2024-01-03 20:46:26 +0800

    Update 0-promise.js

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,11 +1,5 @@
 // 0-promise.js
 
-function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
-    // You can perform asynchronous operations here
-    // For simplicity, let's resolve the Promise immediately
-    resolve('API response data');
-  });
+export default function getResponseFromAPI() {
+  return new Promise(() => {});
 }
-
-export default getResponseFromAPI;
