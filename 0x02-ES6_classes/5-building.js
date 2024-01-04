519e1f7d324a613170edb58ba12097f0ce15232f
InfintyJer <89025169+InfinityJer@users.noreply.github.com>
2024-01-04 18:33:09 +0800

    Update 5-building.js

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,17 +1,29 @@
 // 5-building.js
-export default class Building {
+class Building {
   constructor(sqft) {
-    // Store attributes in underscore attribute version
+    if (this.constructor !== Building) {
+      if (typeof this.evacuationWarningMessage !== 'function') {
+        throw new Error(
+          'Class extending Building must override evacuationWarningMessage',
+        );
+      }
+    }
+    this.sqft = sqft;
+  }
+
+  /**
+   * @param {Number} sqft
+   */
+  set sqft(sqft) {
+    if (typeof sqft !== 'number') {
+      throw new TypeError('sqft nust be a number');
+    }
     this._sqft = sqft;
   }
 
-  // Getter for sqft attribute
   get sqft() {
     return this._sqft;
   }
-
-  // Abstract method, to be implemented by subclasses
-  evacuationWarningMessage() {
-    throw new Error('Class extending Building must override evacuationWarningMessage');
-  }
 }
+
+export default Building;
