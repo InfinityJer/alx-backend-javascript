8eb6655e1d142791d81b0d30efb267c71574a52d
InfintyJer <89025169+InfinityJer@users.noreply.github.com>
2024-01-04 17:52:28 +0800

    Update 1-make_classrooms.js

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -2,14 +2,11 @@
 import ClassRoom from './0-classroom.js';
 
 function initializeRooms() {
-  // Create an array of ClassRoom objects with sizes 19, 20, and 34
-  const rooms = [
-    new ClassRoom(19),
-    new ClassRoom(20),
-    new ClassRoom(34),
+  return [
+    new Classroom(19),
+    new Classroom(20),
+    new Classroom(34),
   ];
-
-  return rooms;
 }
 
 export default initializeRooms;
