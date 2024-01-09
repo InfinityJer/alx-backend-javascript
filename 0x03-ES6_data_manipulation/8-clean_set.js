25c0bf7fdf80e4fbcff6cb93a1f4996496b6daa9
InfinityJer <jclaris6@gmail.com>
2024-01-09 12:14:40 +0000

    8. Clean set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -3,10 +3,10 @@
 // Function to clean a set and return a string of values starting with a specific string
 function cleanSet(set, startString) {
   // Use the filter method to get values starting with the specified string
-  const filteredValues = Array.from(set).filter(value => value.startsWith(startString));
+  const filteredValues = Array.from(set).filter((value) => value.startsWith(startString));
 
   // Use the map method to append the rest of the string after the startString
-  const cleanedValues = filteredValues.map(value => value.slice(startString.length));
+  const cleanedValues = filteredValues.map((value) => value.slice(startString.length));
 
   // Join the cleaned values with hyphens and return the result
   return cleanedValues.join('-');
