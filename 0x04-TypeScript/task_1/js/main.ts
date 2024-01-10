59a6ea12478f3dc119e5790438052ae827cbaf58
InfinityJer <jclaris6@gmail.com>
2024-01-10 20:05:59 +0000

    Let's build a Teacher interface

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,20 @@
+// Define the Teacher interface
+interface Teacher {
+  firstName: string;
+  lastName: string;
+  readonly fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  readonly location: string;
+  [key: string]: any; // Allow any additional attribute
+}
+
+// Create a Teacher object
+const teacher3: Teacher = {
+  firstName: 'John',
+  fullTimeEmployee: false,
+  lastName: 'Doe',
+  location: 'London',
+  contract: false,
+};
+
+console.log(teacher3);
