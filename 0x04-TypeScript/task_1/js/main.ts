0ebf9d2e0428404f7949cdc2396206f4cbbcbb93
InfinityJer <jclaris6@gmail.com>
2024-01-10 20:18:22 +0000

    updated  main.ts

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,20 +1,56 @@
 // Define the Teacher interface
-interface Teacher {
-  firstName: string;
-  lastName: string;
-  readonly fullTimeEmployee: boolean;
+export interface Teacher {
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
   yearsOfExperience?: number;
-  readonly location: string;
-  [key: string]: any; // Allow any additional attribute
+  location: string;
+  [index: string]: any; // Allow any additional attribute
 }
 
-// Create a Teacher object
-const teacher3: Teacher = {
-  firstName: 'John',
-  fullTimeEmployee: false,
-  lastName: 'Doe',
-  location: 'London',
-  contract: false,
-};
+// Define the Director interface extending Teacher
+export interface Director extends Teacher {
+  numberOfReport: number;
+}
 
-console.log(teacher3);
+// Define the printTeacherFunction interface
+export interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+// Define the printTeacher function
+export function printTeacher(firstName: string, lastName: string): string {
+  return `${firstName[0]}. ${lastName}`;
+}
+
+// Define the IStudentClassConstructor interface for the constructor of StudentClass
+export interface IStudentClassConstructor {
+  new (firstName: string, lastName: string): IStudentClass;
+}
+
+// Define the IStudentClass interface for the StudentClass class
+export interface IStudentClass {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+// Implement the StudentClass class
+export class StudentClass implements IStudentClass {
+  private _firstName!: string;
+  private _lastName!: string;
+
+  constructor(firstName: string, lastName: string) {
+    this._firstName = firstName;
+    this._lastName = lastName;
+  }
+
+  // Implement the workOnHomework method
+  workOnHomework() {
+    return 'Currently working';
+  }
+
+  // Implement the displayName method
+  displayName() {
+    return this._firstName;
+  }
+}
