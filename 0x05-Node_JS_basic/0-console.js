9d29eca58adfdf93ed2ce2945cbdcb9c3aa9bb29
InfinityJer <jclaris6@gmail.com>
2024-02-29 10:04:08 +0000

    Executing basic javascript with Node JS

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+// File: 0-console.js
+
+// Define a function named displayMessage that takes a message parameter
+function displayMessage(message) {
+    // Print the message to the standard output
+    console.log(message);
+}
+
+// Export the displayMessage function to make it accessible from other modules
+module.exports = displayMessage;
