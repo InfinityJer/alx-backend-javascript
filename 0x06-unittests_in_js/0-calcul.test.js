444956278379ebe5278afe55e5863bef65a89222
InfintyJer <89025169+InfinityJer@users.noreply.github.com>
2024-03-02 11:33:00 +0300

    Update 0-calcul.test.js

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -8,4 +8,11 @@ describe('calculateNumber', () => {
         assert.strictEqual(calculateNumber(1.2, 3.7), 5);
         assert.strictEqual(calculateNumber(1.5, 3.7), 6);
     });
+
+    it('should return the sum of two rounded numbers for the second number rounded', () => {
+        assert.strictEqual(calculateNumber(3, 1), 4);
+        assert.strictEqual(calculateNumber(3.7, 1), 5);
+        assert.strictEqual(calculateNumber(3.7, 1.2), 5);
+        assert.strictEqual(calculateNumber(3.7, 1.5), 6);
+    });
 });
